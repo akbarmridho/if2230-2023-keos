dcdfcaa5f99f907599e85befd99e508acafd1b33
arsaizdihar <arsadihar@gmail.com>
2023-02-18 23:31:47 +0700

    framebuffer clear

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -12,7 +12,6 @@ void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
 {
     uint8_t *position = MEMORY_FRAMEBUFFER + COLUMN * 2 * row + col * 2;
     uint16_t attributes = (bg << 4) | (fg & 0x0F);
-    // uint16_t content = c | (attributes << 8);
 
     memset(position, c, 1);
     memset(position + 1, attributes, 1);
@@ -20,5 +19,11 @@ void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
 
 void framebuffer_clear(void)
 {
-    // TODO : Implement
+    for (size_t i = 0; i < ROW; i++)
+    {
+        for (size_t j = 0; j < COLUMN; j++)
+        {
+            framebuffer_write(i, j, '\0', 0, 0);
+        }
+    }
 }
