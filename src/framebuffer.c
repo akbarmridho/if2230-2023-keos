39d41894f1f87aac44121076d9bddeda70e78a1b
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-02-18 23:24:46 +0700

    fix framebuffer

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -10,11 +10,12 @@ void framebuffer_set_cursor(uint8_t r, uint8_t c)
 
 void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
 {
-    uint8_t *position = MEMORY_FRAMEBUFFER + COLUMN * row + col;
+    uint8_t *position = MEMORY_FRAMEBUFFER + COLUMN * 2 * row + col * 2;
     uint16_t attributes = (bg << 4) | (fg & 0x0F);
-    uint16_t content = c | (attributes << 8);
+    // uint16_t content = c | (attributes << 8);
 
-    memset(position, content, 2);
+    memset(position, c, 1);
+    memset(position + 1, attributes, 1);
 }
 
 void framebuffer_clear(void)
