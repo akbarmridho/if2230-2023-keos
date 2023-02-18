3decabeb2f4ef245b0ce6334b5ae6f94e6d438b1
Fakhri Muhammad Mahendra <16521155@mahasiswa.itb.ac.id>
2023-02-18 22:45:54 +0700

    add framebuffer.c

---
+++ src/framebuffer.c
@@ -0,0 +1,16 @@
+#include "lib-header/framebuffer.h"
+#include "lib-header/stdtype.h"
+#include "lib-header/stdmem.h"
+#include "lib-header/portio.h"
+
+void framebuffer_set_cursor(uint8_t r, uint8_t c) {
+    // TODO : Implement
+}
+
+void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
+    // TODO : Implement
+}
+
+void framebuffer_clear(void) {
+    // TODO : Implement
+}
