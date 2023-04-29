e574141dc5d61b217caa9ab0ea652fe28633cf62
Fakhri Muhammad Mahendra <16521155@mahasiswa.itb.ac.id>
2023-04-29 13:51:21 +0700

    doc: add doc to next_arg function

--- src/user/command.c
+++ src/user/command.c
@@ -3,24 +3,26 @@
 
 void command_input(char *buf, char *currentdir, uint32_t size)
 {
-  puts_color("Keos@OS-IF2230", 0x2);
-  puts_color(":", 0x7);
-  puts_color(currentdir, 0x9);
-  puts_color("$ ", 0x7);
-  gets(buf, size);
+    puts_color("Keos@OS-IF2230", 0x2);
+    puts_color(":", 0x7);
+    puts_color(currentdir, 0x9);
+    puts_color("$ ", 0x7);
+    gets(buf, size);
 }
-
+/**
+ * @brief Get the next arg object
+ */
 void next_arg(char **pstr, uint8_t *len)
 {
-  *len = 0;
-  while ((**pstr) == ' ')
-  {
-    if (**pstr == '\0')
-      return;
-    *pstr += 1;
-  }
-  while ((*pstr)[*len] != ' ' && (*pstr)[*len] != '\0')
-  {
-    *len += 1;
-  }
+    *len = 0;
+    while ((**pstr) == ' ')
+    {
+        if (**pstr == '\0')
+            return;
+        *pstr += 1;
+    }
+    while ((*pstr)[*len] != ' ' && (*pstr)[*len] != '\0')
+    {
+        *len += 1;
+    }
 }
