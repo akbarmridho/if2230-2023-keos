5de508d4b77f43a965e9f226ffec169151d54a43
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-04-29 15:27:23 +0700

    feat: lcg rand implementation

--- src/user/utils.c
+++ src/user/utils.c
@@ -4,7 +4,7 @@ void waitsecond()
 {
     uint32_t current_timestamp = get_timestamp();
 
-    while (current_timestamp == get_timestamp())
+    while (get_timestamp() - current_timestamp > 0)
         ;
 }
 
@@ -13,4 +13,20 @@ void waitbusy()
     // loop 1 million time
     for (uint32_t i = 0; i < 1e6; i++)
         ;
+}
+
+static uint32_t next = 1;
+
+int32_t rand(int32_t low, int32_t high)
+{
+    next = next * 1103515245 + 12345;
+
+    int32_t initial_result = (unsigned int)(next / 65536) % 32768;
+
+    return (initial_result % (high - low + 1)) + low;
+}
+
+void srand(uint32_t seed)
+{
+    next = seed;
 }
--- src/user/utils.h
+++ src/user/utils.h
@@ -1,7 +1,11 @@
+#include "../lib-header/stdtype.h"
+
 /**
  * Wait until second changes
- * Time wait vary between 0ms to 1000ms
+ * Time wait vary between 1000ms to 2000ms
  */
 void waitsecond();
 
 void waitbusy();
+
+int32_t rand(int32_t low, int32_t high);
